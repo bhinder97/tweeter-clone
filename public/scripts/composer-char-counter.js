14131e8ca2e9ddd85f57d09488a9aa1f67946cfc
Harsimran Bhinder <bhinderrr@gmail.com>
2022-04-26 22:08:13 +0000

    updated char count code

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -34,5 +34,14 @@ $(document).ready(function() {
       $(log).css('color', 'black');
     }
   });
+
+  // let charCounter = 0;
+  // $('textarea').on('input', () => {
+  //   charCounter = $('textarea').val().length;
+  //   $('.counter').text(140 - charCounter);
+  //   if (charCounter > 140) {
+  //     $('.counter').css('color', 'red');
+  //   }
+  // });
   
 });
