aa03af36c8de11dceeb37170adeb4cfde48acd1a
Harsimran Bhinder <bhinderrr@gmail.com>
2022-04-26 21:57:39 +0000

    character counter updates

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,38 @@
+$(document).ready(function() {
+
+  //vanilla JS
+  
+  // const input = document.querySelector('textarea');
+  // const log = document.getElementById('counter'); //elemsbyClassName returns an ARRAY
+
+  // const changeValue = function(x) {
+  //   log.textContent = x.target.value;
+  //   let count = log.textContent.length;
+  //   let char = 140 - count;
+  //   document.getElementById('counter').innerHTML = char; //elembyID returns singular value, better to use than elemsbyClassNAme
+  //   if (char < 0) {
+  //     document.getElementById('counter').style.color = "red";
+  //   } else {
+  //     document.getElementById('counter').style.color = "black";
+  //   }
+  // }
+  // input.addEventListener('input', changeValue);
+
+
+  //jQuery way
+
+
+  const log = $('.counter'); //use . to pull class names
+  $('textarea').on('input', (x) => { //listens to input events on textarea HTML
+    let count = x.target.value.length;
+    let char = 140;
+    char = 140 - count;
+    $(log).text(char);
+    if (char < 0) {
+      $(log).css('color', 'red');
+    } else {
+      $(log).css('color', 'black');
+    }
+  });
+  
+});
