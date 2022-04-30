d3fae181a8278b682331063f90f376c5501129b3
Harsimran Bhinder <bhinderrr@gmail.com>
2022-04-30 02:24:38 +0000

    final touchups

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,27 +1,4 @@
 $(document).ready(function() {
-
-  //vanilla JS - leaving this here to show how to do it the other way
-  
-  // const input = document.querySelector('textarea');
-  // const log = document.getElementById('counter'); //elemsbyClassName returns an ARRAY
-
-  // const changeValue = function(x) {
-  //   log.textContent = x.target.value;
-  //   let count = log.textContent.length;
-  //   let char = 140 - count;
-  //   document.getElementById('counter').innerHTML = char; //elembyID returns singular value, better to use than elemsbyClassNAme
-  //   if (char < 0) {
-  //     document.getElementById('counter').style.color = "red";
-  //   } else {
-  //     document.getElementById('counter').style.color = "black";
-  //   }
-  // }
-  // input.addEventListener('input', changeValue);
-
-
-  //jQuery way
-
-
   const log = $('.counter'); //use . to pull class names
   $('textarea').on('input', (x) => { //listens to input events on textarea HTML
     let count = x.target.value.length;
